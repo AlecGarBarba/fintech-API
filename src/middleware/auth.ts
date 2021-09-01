f96dc008485f5d3d6baaa9769e2eb90841d225a1
unknown <alecbarba@gmail.com>
2021-08-31 23:39:24 -0500

    refactor de funcion de auth

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,13 +1,11 @@
 import { Request, Response } from 'express'; 
-import jwt from 'jsonwebtoken';
-//import DataStorager from '../models/DataStorager';
+import jwt from 'jsonwebtoken'; 
 
 export const auth = async (req: Request, res: Response, next: Function)=>{
     try{
         const token = req.header('Authorization')?.replace('Bearer ','');
-        const decoded = jwt.verify(token!, process.env.JWT_SECRET as jwt.Secret) as jwt.JwtPayload;
-        const {nombre} = decoded;
-        req.params.nombreUsuario = nombre;
+        const decoded = jwt.verify(token!, process.env.JWT_SECRET as jwt.Secret) as jwt.JwtPayload; 
+        req.params.nombreUsuario = decoded.nombre;
        next(); 
     }catch(e){
         res.status(401).send({error: `Error de autenticación: ${e.message}`});
