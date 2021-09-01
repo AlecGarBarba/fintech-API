c9eddec2ccb89b684fbe6ba394242359e7be4155
unknown <alecbarba@gmail.com>
2021-09-01 09:34:33 -0500

    Adicion filtros para listado transacciones totales y por cuenta

--- src/routes/routerClientes.ts
+++ src/routes/routerClientes.ts
@@ -31,7 +31,7 @@ router.post('/cuenta', auth, async (req,res)=>{
 
 
 
-//Agregar transaccion a una cuenta específica*
+//Agregar transaccion a una cuenta específica
 router.post('/cuenta/transaccion',auth, async (req,res)=>{
     const { nombreUsuario } = req.params;
     const { nombreCliente, idCuenta,cantidad, concepto, tipoDeTransaccion } = req.body;
@@ -84,15 +84,31 @@ router.get('/cuentas',auth, async(req,res)=>{
 //obtener todas las transacciones del cliente, independientemente de su cuenta. Filtro por fechas*
 router.get('/transacciones',auth, async(req,res)=>{
     const nombreUsuario = req.params.nombreUsuario;
+    const {inicio, fin} = req.query; 
     const usuario = await DataStorager.retornarUsuario(nombreUsuario);
     let transacciones: Transaccion[] = [];
-    if(usuario){
-        usuario.clientes.forEach((cliente)=>{
-            cliente.cuentas.forEach(cuenta=>{
-                transacciones = [...transacciones, ...cuenta.transacciones]
-            })
-        })
 
+    if(usuario){
+
+        usuario.clientes.forEach((cliente)=>{ 
+            cliente.cuentas.forEach(cuenta=>{ 
+                transacciones = [...transacciones, ...cuenta.transacciones]
+            });
+        });
+
+        if(inicio && fin){
+            transacciones = transacciones.filter( (transaccion)=>{
+                return new Date(transaccion.fechaTransaccion) >= new Date(''+inicio) && new Date(transaccion.fechaTransaccion) <= new Date(''+fin)
+            })
+        }else if(inicio){
+            transacciones = transacciones.filter( (transaccion)=>{
+                return new Date(transaccion.fechaTransaccion) >= new Date(''+inicio)  
+            })
+        }else if(fin){
+            transacciones = transacciones.filter( (transaccion)=>{
+                return new Date(transaccion.fechaTransaccion) <= new Date(''+fin)  
+            })
+        } 
     }
     
     res.status(200).send(transacciones);
@@ -102,13 +118,30 @@ router.get('/transacciones',auth, async(req,res)=>{
 //obtener transacciones por cuenta. Filtro por fecha*
 router.get('/cuenta/transacciones', auth, async (req,res)=>{
     const { nombreUsuario} = req.params; 
-    const { nombreCliente, idCuenta } = req.query;
+    const { nombreCliente, idCuenta, inicio, fin } = req.query;
 
     const usuario = await DataStorager.retornarUsuario(nombreUsuario);
     if(usuario && nombreCliente && idCuenta ){
+        let transacciones: Transaccion[]= [];
         const {cuentas} = usuario.retornarCliente(nombreCliente as string) as Cliente;
         const cuentaFiltrada = cuentas.filter( cuenta => cuenta.id=== idCuenta );
-        res.status(201).send(cuentaFiltrada[0].transacciones );
+        transacciones = cuentaFiltrada[0].transacciones
+
+        if(inicio && fin){
+            transacciones = transacciones.filter( (transaccion)=>{
+                return new Date(transaccion.fechaTransaccion) >= new Date(''+inicio) && new Date(transaccion.fechaTransaccion) <= new Date(''+fin)
+            })
+        }else if(inicio){
+            transacciones = transacciones.filter( (transaccion)=>{
+                return new Date(transaccion.fechaTransaccion) >= new Date(''+inicio)  
+            })
+        }else if(fin){
+            transacciones = transacciones.filter( (transaccion)=>{
+                return new Date(transaccion.fechaTransaccion) <= new Date(''+fin)  
+            })
+        } 
+
+        res.status(201).send( transacciones );
     }else{
         res.status(400).send({"error": "Datos insuficientes"});
     }
