984533300534385c2d666d676f88bbb3271a1fdf
unknown <alecbarba@gmail.com>
2021-09-01 01:23:30 -0500

    router cliente agregado a función principal

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { Response } from 'express';
 import routerUsuario from './routes/routerUsuario';
+import routerClientes from './routes/routerClientes';
 function main(): void {
     const app = express();
 app.use(express.json());
     app.use('/user', routerUsuario);
+    app.use('/clientes', routerClientes);
 
     app.get('/', (_, res: Response)=>{
         res.send("Hola, mundo!");
